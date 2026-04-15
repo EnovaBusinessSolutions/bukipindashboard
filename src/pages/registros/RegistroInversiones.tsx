01c52c9c92af17a2c0214e57ffcb22a196b08da7
Jose Manuel Mejia <josemanuelmejiadominguez2@gmail.com>
2026-04-14 23:40:17 -0600

    mod init

--- src/pages/registros/RegistroInversiones.tsx
+++ src/pages/registros/RegistroInversiones.tsx
@@ -41,7 +41,7 @@ const RegistroInversiones = () => {
             <TabsTrigger value="analitica">Analítica</TabsTrigger>
           </TabsList>
 
-          <TabsContent value="registro" className="mt-6">
+          <TabsContent value="registro" className="mt-6 flex-1 min-h-0 overflow-y-auto pr-1">
             <RegistroInversionForm />
           </TabsContent>
 
